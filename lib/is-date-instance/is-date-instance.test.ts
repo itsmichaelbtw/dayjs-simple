888e7d4c71471c2c6345e35ffa7477c83ada2649
Michael <me@miikey.pw>
2022-09-28 20:00:54 +1000

    test: renamed test suites for better understanding

--- lib/is-date-instance/is-date-instance.test.ts
+++ lib/is-date-instance/is-date-instance.test.ts
@@ -1,29 +1,36 @@
 import { describe, test, expect } from "@jest/globals";
-import { isDateInstance } from "./index";
+import { isDateInstance, MutableDate } from "../index";
 
 describe("isDateInstance", () => {
-    test("null", () => {
+    test("null should be false", () => {
         const result = isDateInstance(null);
         expect(result).toBe(false);
     });
 
-    test("undefined", () => {
+    test("undefined should be false", () => {
         const result = isDateInstance(undefined);
         expect(result).toBe(false);
     });
 
-    test("new Date", () => {
+    test("new Date should be true", () => {
         const result = isDateInstance(new Date());
         expect(result).toBe(true);
     });
 
-    test("YYYY-MM-DD", () => {
+    test("current timestamp should be true", () => {
+        const currentTimestamp = new Date().getTime();
+        const result = isDateInstance(currentTimestamp);
+        expect(result).toBe(false);
+    });
+
+    test("2021-01-01 should be true", () => {
         const result = isDateInstance("2021-01-01");
         expect(result).toBe(false);
     });
 
-    test("timestamp", () => {
-        const result = isDateInstance(1612137600000);
-        expect(result).toBe(false);
+    test("2021-01-01 as MutableDate should be true", () => {
+        const date = new MutableDate("2021-01-01");
+        const result = isDateInstance(date.toDate());
+        expect(result).toBe(true);
     });
 });
--- lib/is-date-valid/is-date-valid.test.ts
+++ lib/is-date-valid/is-date-valid.test.ts
@@ -1,39 +1,51 @@
 import { describe, test, expect } from "@jest/globals";
-import { isDateValid } from "./index";
+import { MutableDate, isDateValid } from "../index";
 
 describe("isDateValid", () => {
-    test("null", () => {
+    test("null should be false", () => {
         const result = isDateValid(null);
         expect(result).toBe(false);
     });
 
-    test("undefined", () => {
+    test("undefined should be false", () => {
         const result = isDateValid(undefined);
         expect(result).toBe(false);
     });
 
-    test("new Date", () => {
+    test("new Date should be true", () => {
         const result = isDateValid(new Date());
         expect(result).toBe(true);
     });
 
-    test("YYYY-MM-DD", () => {
+    test("current timestamp should be true", () => {
+        const currentTimestamp = new Date().getTime();
+        const result = isDateValid(currentTimestamp);
+        expect(result).toBe(true);
+    });
+
+    test("2021-01-1 should be true", () => {
         const result = isDateValid("2021-01-01");
         expect(result).toBe(true);
     });
 
-    test("timestamp", () => {
-        const result = isDateValid(1612137600000);
-        expect(result).toBe(true);
-    });
-
-    test("invalid date", () => {
+    test("2021-01-31 should be false", () => {
         const result = isDateValid("2021-01-32");
         expect(result).toBe(false);
     });
 
-    test("invalid time", () => {
+    test("2021-01-01 as MutableDate should be true", () => {
+        const date = new MutableDate("2021-01-01");
+        const result = isDateValid(date);
+        expect(result).toBe(true);
+    });
+
+    test("2021-01-01T25:00:00 should be false", () => {
         const result = isDateValid("2021-01-01T25:00:00");
+        expect(result).toBe(false);
+    });
+
+    test(`'invalid date' should be false`, () => {
+        const result = isDateValid(new Date("invalid date"));
         expect(result).toBe(false);
     });
 });
