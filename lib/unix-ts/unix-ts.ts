21014424a3310f0dbd2d396ba3cc60907240176b
Michael <me@miikey.pw>
2022-09-28 21:13:27 +1000

    refactor: removed dayjs reference from method

--- lib/unix-ts/unix-ts.ts
+++ lib/unix-ts/unix-ts.ts
@@ -1,11 +1,38 @@
-import dayjs from "dayjs";
+import { MutableDate } from "../mutable-date";
+import { isDateValid } from "../is-date-valid";
+import { argumentIsNotDefined } from "../utils";
 
-type UnixUnit = "milliseconds" | "seconds";
+import { UnixUnitConversion, MutableDateArgument } from "../types";
 
-export function unixTs(time: number, unit: UnixUnit): dayjs.Dayjs {
+function computeUnix(date: Date, unit: UnixUnitConversion) {
     if (unit === "milliseconds") {
-        return dayjs.unix(time / 1000);
+        return date.getTime();
     }
 
-    return dayjs.unix(time);
+    return Math.floor(date.getTime() / 1000);
+}
+
+/**
+ * returns the unix timestamp of the given date. If no date is given, the current date is used.
+ */
+
+export function unixTs(
+    date?: MutableDateArgument,
+    unit: UnixUnitConversion = "milliseconds"
+): number {
+    const unixNow = computeUnix(new Date(), unit);
+
+    if (argumentIsNotDefined(date)) {
+        return unixNow;
+    }
+
+    if (MutableDate.isInstance(date)) {
+        return date.unix(unit);
+    }
+
+    if (isDateValid(date)) {
+        return computeUnix(new Date(date), unit);
+    }
+
+    return unixNow;
 }
