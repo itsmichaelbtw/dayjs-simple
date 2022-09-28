0ed3754d576a1e5b6bee0fdabbd07c6efa5fffc5
Michael <me@miikey.pw>
2022-09-28 20:40:18 +1000

    refactor: dayjs extension plugins

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,14 @@
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import isBetween from "dayjs/plugin/isBetween";
+import isLeapYear from "dayjs/plugin/isLeapYear";
+
+dayjs.extend(utc);
+dayjs.extend(isBetween);
+dayjs.extend(isLeapYear);
+
+export * from "./mutable-date";
+export * from "./is-date-instance";
+export * from "./is-date-valid";
+export * from "./unix-ts";
+export * from "./increment-date";
