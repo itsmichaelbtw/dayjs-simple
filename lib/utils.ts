536731aede6d30238131466dd4be4bb165945f19
Michael <me@miikey.pw>
2022-10-02 15:55:28 +1100

    feat: exportable utility helpers

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
-import { MutableDate } from "./mutable-date";
-import { ObjectTypes, OutputConversionOptions } from "./types";
+import { isDateInstance } from "./is-date-instance";
+
+import { ObjectTypes } from "./types";
 
 const toObjectString = Object.prototype.toString;
 
@@ -10,5 +11,52 @@ export function callObjectType(input: any): ObjectTypes {
 }
 
 export function argumentIsNotDefined(input: any): boolean {
-    return input === null || input === undefined;
+    return input === undefined || input === null;
+}
+
+export function swapArguments<T>(arg1: T, arg2: T): [T, T] {
+    return [arg2, arg1];
+}
+
+export function isNegative(input: number): boolean {
+    return input < 0;
+}
+
+export function isPositive(input: number): boolean {
+    return input > 0;
+}
+
+export function absolute(input: number): number {
+    return Math.abs(input);
+}
+
+export function round(number: number): number {
+    if (isNegative(number)) {
+        return Math.ceil(number);
+    }
+
+    return Math.floor(number);
+}
+
+export function getTimezoneOffset(date?: Date): number {
+    let offset: number = 0;
+
+    if (isDateInstance(date)) {
+        offset = date.getTimezoneOffset();
+    } else {
+        offset = new Date().getTimezoneOffset();
+    }
+
+    return -round(offset / 15) * 15;
+}
+
+export function getTimezoneString(): string {
+    if (
+        typeof Intl !== "undefined" &&
+        typeof Intl.DateTimeFormat !== "undefined"
+    ) {
+        return new Intl.DateTimeFormat().resolvedOptions().timeZone;
+    }
+
+    return "UTC";
 }
