458beeb5f18fae55bd0a0cc8d72cc4fb6dd5b1ab
Michael <me@miikey.pw>
2022-10-02 15:53:46 +1100

    chore: project constants

---
+++ lib/constants.ts
@@ -0,0 +1,31 @@
+export const ISO_8601 = "YYYY-MM-DDTHH:mm:ssZ";
+export const ISO_8601_WITH_TZ_OFFSET = "YYYY-MM-DDTHH:mm:ssZZ";
+
+export const SECONDS_IN_MINUTE = 60;
+export const SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTE;
+export const SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR;
+export const SECONDS_IN_WEEK = 7 * SECONDS_IN_DAY;
+export const SECONDS_IN_MONTH = 30 * SECONDS_IN_DAY;
+export const SECONDS_IN_YEAR = 365 * SECONDS_IN_DAY;
+
+export const MILLIS_IN_SECOND = 1000;
+export const MILLIS_IN_MINUTE = SECONDS_IN_MINUTE * MILLIS_IN_SECOND;
+export const MILLIS_IN_HOUR = SECONDS_IN_HOUR * MILLIS_IN_SECOND;
+export const MILLIS_IN_DAY = SECONDS_IN_DAY * MILLIS_IN_SECOND;
+export const MILLIS_IN_WEEK = SECONDS_IN_WEEK * MILLIS_IN_SECOND;
+export const MILLIS_IN_MONTH = SECONDS_IN_MONTH * MILLIS_IN_SECOND;
+export const MILLIS_IN_YEAR = SECONDS_IN_YEAR * MILLIS_IN_SECOND;
+
+export const LOCALE = {
+    MS: "millisecond",
+    S: "second",
+    MIN: "minute",
+    H: "hour",
+    D: "day",
+    W: "week",
+    M: "month",
+    Q: "quarter",
+    Y: "year"
+};
+
+export const INVALID_DATE = "Invalid Date";
