e53058fc338039d932d3c3e5a051dfb6444c464a
Michael <me@miikey.pw>
2022-10-13 13:37:31 +1100

    refactor: migrated top-level code to index.ts (removed redundant files)

--- lib/increment-date/index.ts
+++ lib/increment-date/index.ts
@@ -1,1 +1,29 @@
-export * from "./increment-date";
+import { isDateValid } from "../is-date-valid";
+import { MutableDate } from "../mutable-date";
+
+import type {
+    ManipulateType,
+    MutableDateArgument,
+    OutputConversionOptions
+} from "../types";
+
+/**
+ * Increment a given date by a given amount of time. If an invalid date is provided, the current date is used.
+ * Passing an instance of MutableDate will mutate the original instance.
+ */
+export function incrementDate(
+    date: MutableDateArgument,
+    value: number,
+    unit: ManipulateType,
+    outputConversion?: OutputConversionOptions
+): MutableDate {
+    if (MutableDate.isInstance(date)) {
+        return date.add(value, unit);
+    }
+
+    if (isDateValid(date)) {
+        return MutableDate.create(date).add(value, unit);
+    }
+
+    return MutableDate.create().add(value, unit);
+}
